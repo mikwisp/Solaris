7533b3f908b2b34e789855b90b3e32b13bc03871
BluBerry016 <50649185+unit0016@users.noreply.github.com>
2025-04-19 16:49:05 -0400

    GREP Adjustment (#6)
    * fix grep for modular folders
    
    i want to deprecate these longterm but they shouldn't be excluded from CI either. I don't want any nasty surprises.
    
    * newline reee

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -19,13 +19,13 @@ if command -v rg >/dev/null 2>&1; then
 	if [ ! rg -P '' >/dev/null 2>&1 ] ; then
 		pcre2_support=0
 	fi
-	code_files="code/**/**.dm modular_hearthstone/**/*.dm modular_helmsguard/**/*.dm modular_stonehedge/**/*.dm"
+	code_files="code/**/**.dm modular_hearthstone/**/*.dm modular_azurepeak/**/*.dm"
 	map_files="_maps/**/**.dmm"
 	code_x_515="code/**/!(__byond_version_compat).dm"
 else
 	pcre2_support=0
 	grep=grep
-	code_files="-r --include=\"code/**/**.dm modular_hearthstone/**/*.dm modular_helmsguard/**/*.dm modular_stonehedge/**/*.dm\""
+	code_files="-r --include=\"code/**/**.dm modular_hearthstone/**/*.dm modular_azurepeak/**/*.dm\""
 	map_files="-r --include=_maps/**/**.dmm"
 	code_x_515="-r --include=code/**/!(__byond_version_compat).dm"
 fi
